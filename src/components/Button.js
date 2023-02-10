6523926253299f2ff05de6b1245ac3a354004e83
sanketh <sanket@expertia.ai>
2023-02-10 15:43:31 +0530

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import "./index.css";
+
+export const Button = ({ children, className, ...rest }) => {
+  const combined = twMerge(
+    "text-white bg-[#6D5DE7] hover:bg-primary2 duration-200 font-md rounded-lg text-sm py-2 px-4",
+    className
+  );
+
+  return (
+    <button
+      className={combined}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
