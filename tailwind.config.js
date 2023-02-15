5c185b0c74d6cf8f8f61e639ebcded64d1c62f3d
sanketh <sanket@expertia.ai>
2023-02-15 12:23:29 +0530

    c21

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors:{
+      primary1:'#6D5DE7',
+      primary2:'#574ab8',
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...colors
+    }
   },
   plugins: [],
 }
